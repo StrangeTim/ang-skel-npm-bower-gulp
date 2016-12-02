cde6a067c24f4afe611cc6bf34232cc424864a56
StrangeTim <strang3tim@gmail.com>
2016-12-02 11:15:09 -0800

    finish angular skeleton build

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+// THIS IS THE COMPONENT ANNOTATION SECTION
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>My First Angular 2 App</h1>
+  `
+})
+
+
+//THIS IS THE COMPONENT CLASS DECLARATION
+export class AppComponent {
+
+}
